07de30bdc9c3464fc5a60af852c4f0d6cf2d75b3
Alan <danghoanganh1975@gmail.com>
2023-09-22 23:35:11 +0700

    ss

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*
+float tong( float a, float b) {
+    float tong = a + b;
+    return tong;
+}
+int main(int argc, char **argv){
+    printf("tong la : %f\n",tong(4,6));
+     * */
+    /*
+ float hieu( float a, float b) {
+    float hieu = a - b;
+    return hieu;
+}
+int main(int argc, char **argv){
+    printf("hieu la : %f\n",hieu(6,4));
+     */
+    /* float tich( float a, float b) {
+    float tich = a * b;
+    return tich;
+}
+int main(int argc, char **argv){
+    printf("tich la : %f\n",tich(4,6));
+    */
+    /*
+    float thuong( float a, float b) {
+    float thuong = a / b;
+    return thuong;
+}
+int main(int argc, char **argv){
+    printf("thuong la : %f\n",thuong(4,6));
+     * */
+    getchar();
+	return 0;
+}
